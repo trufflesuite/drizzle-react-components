181a77df8f95feb656b1f8e223b176ab39f0f5e3
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2018-07-05 16:09:00 +0300

    Fixed ContractData rendering of array data

--- src/ContractData.js
+++ src/ContractData.js
@@ -65,11 +65,11 @@ class ContractData extends Component {
     }
 
     // If return value is an array
-    if (typeof displayData === 'array') {
+    if (Array.isArray(displayData)) {
       const displayListItems = displayData.map((datum, index) => {
-        <li key={index}>{datum}{pendingSpinner}</li>
+        return <li key={index}>{datum}{pendingSpinner}</li>
       })
-      
+
       return(
         <ul>
           {displayListItems}
