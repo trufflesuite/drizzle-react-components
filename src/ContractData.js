d185a0da3efa4416691427868788e56524df8112
Sam Ruberti <samruberti@gmail.com>
2018-10-07 10:47:49 -0400

    Fix broken type comparison

--- src/ContractData.js
+++ src/ContractData.js
@@ -56,7 +56,7 @@ class ContractData extends Component {
     }
 
     // If return value is an array
-    if (typeof displayData === 'array') {
+    if (Array.isArray(displayData)) {
       const displayListItems = displayData.map((datum, index) => (
         <li key={index}>{`${datum}`}{pendingSpinner}</li>
       ))
