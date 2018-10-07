8ea8129a0a0144f1504c0d86d9f5c9a366e6f1b5
Sam Ruberti <samruberti@gmail.com>
2018-10-07 10:29:22 -0400

    Fix missing return in ContractData

--- src/ContractData.js
+++ src/ContractData.js
@@ -57,9 +57,9 @@ class ContractData extends Component {
 
     // If return value is an array
     if (typeof displayData === 'array') {
-      const displayListItems = displayData.map((datum, index) => {
+      const displayListItems = displayData.map((datum, index) => (
         <li key={index}>{`${datum}`}{pendingSpinner}</li>
-      })
+      ))
 
       return(
         <ul>
